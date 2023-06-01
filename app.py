58fae3d105438569a1e59ed0154ea17b90c31fcc
bemarinos <135247962+bemarinos@users.noreply.github.com>
2023-06-01 17:40:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,27 +14,27 @@ app= Flask (__name__)
 def index():
     return render_template('index.html')
 
-#try:
-#    load_dotenv()
-#    cog_key = os.environ.get("COG_SERVICE_KEY")
-#    cog_region = os.environ.get("COG_SERVICE_REGION")      
-#    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
-#except Exception as ex:        
-#    print(ex)
+try:
+    load_dotenv()
+    cog_key = os.environ.get("COG_SERVICE_KEY")
+    cog_region = os.environ.get("COG_SERVICE_REGION")      
+    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'   
+except Exception as ex:        
+    print(ex)
 
 
 
-#@app.route('/', methods=['GET', 'POST'])
-#def home():
-#    if request.method == 'POST':
-#        text = request.form['text']
-        # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
-        #source_language = ''
-        #translated_text = text
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        text = request.form['text']
+        #Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
+        source_language = ''
+        translated_text = text
 
-        #return render_template('index.html', translated_text=translated_text,lang_detected=source_language)
+        return render_template('index.html', translated_text=translated_text,lang_detected=source_language)
     
-    #return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
