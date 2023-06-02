7376a42092ae07866246cb4ac9971304ff2a9f2d
bemarinos <135247962+bemarinos@users.noreply.github.com>
2023-06-01 23:14:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def home():
     if request.method == 'POST':
         #text = request.form['text']
         #Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
-        #source_language = ''
+        source_language = ''
         #translated_text = text
         #source_language = 'en'    
         ##Use the Translator detect function    
