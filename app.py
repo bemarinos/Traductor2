c170ce6f14f4d34176bb72756db644976f948655
bemarinos <135247962+bemarinos@users.noreply.github.com>
2023-06-01 23:12:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ except Exception as ex:
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        text = request.form['text']
+        #text = request.form['text']
         #Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
         #source_language = ''
         #translated_text = text
